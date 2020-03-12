9bea9d15de7325e33e2cf9b28cfcfe802b87518d
Anna <teteyang@bk.ru>
2020-03-12 18:04:36 +0300

    add theme, add delete task

--- app.js
+++ app.js
@@ -36,11 +36,85 @@ const tasks = [
     acc[task._id] = task;
     return acc;
   }, {});
+const themes = {
+  default: {
+    '--base-text-color': '#212529',
+    '--header-bg': '#007bff',
+    '--header-text-color': '#fff',
+    '--default-btn-bg': '#007bff',
+    '--default-btn-text-color': '#fff',
+    '--default-btn-hover-bg': '#0069d9',
+    '--default-btn-border-color': '#0069d9',
+    '--danger-btn-bg': '#dc3545',
+    '--danger-btn-text-color': '#fff',
+    '--danger-btn-hover-bg': '#bd2130',
+    '--danger-btn-border-color': '#dc3545',
+    '--input-border-color': '#ced4da',
+    '--input-bg-color': '#fff',
+    '--input-text-color': '#495057',
+    '--input-focus-bg-color': '#fff',
+    '--input-focus-text-color': '#495057',
+    '--input-focus-border-color': '#80bdff',
+    '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+  },
+  dark: {
+    '--base-text-color': '#212529',
+    '--header-bg': '#343a40',
+    '--header-text-color': '#fff',
+    '--default-btn-bg': '#58616b',
+    '--default-btn-text-color': '#fff',
+    '--default-btn-hover-bg': '#292d31',
+    '--default-btn-border-color': '#343a40',
+    '--default-btn-focus-box-shadow':
+      '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    '--danger-btn-bg': '#b52d3a',
+    '--danger-btn-text-color': '#fff',
+    '--danger-btn-hover-bg': '#88222c',
+    '--danger-btn-border-color': '#88222c',
+    '--input-border-color': '#ced4da',
+    '--input-bg-color': '#fff',
+    '--input-text-color': '#495057',
+    '--input-focus-bg-color': '#fff',
+    '--input-focus-text-color': '#495057',
+    '--input-focus-border-color': '#78818a',
+    '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+  },
+  light: {
+    '--base-text-color': '#212529',
+    '--header-bg': '#fff',
+    '--header-text-color': '#212529',
+    '--default-btn-bg': '#fff',
+    '--default-btn-text-color': '#212529',
+    '--default-btn-hover-bg': '#e8e7e7',
+    '--default-btn-border-color': '#343a40',
+    '--default-btn-focus-box-shadow':
+      '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    '--danger-btn-bg': '#f1b5bb',
+    '--danger-btn-text-color': '#212529',
+    '--danger-btn-hover-bg': '#ef808a',
+    '--danger-btn-border-color': '#e2818a',
+    '--input-border-color': '#ced4da',
+    '--input-bg-color': '#fff',
+    '--input-text-color': '#495057',
+    '--input-focus-bg-color': '#fff',
+    '--input-focus-text-color': '#495057',
+    '--input-focus-border-color': '#78818a',
+    '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+  },
+};
+let lastSelectedTheme = 'default';
 
 //elem ui
 
 const listContainer = document.querySelector('.tasks-list-section .list-group');
-
+const form = document.forms['addTask'];
+const inputTitle = form.elements['title'];
+const inputBody = form.elements['body'];
+const themeSelect = document.getElementById('themeSelect');
+themeSelect.addEventListener('change', onThemeSelect);
+//events
+form.addEventListener('submit', onFormSubmitHandler);
+listContainer.addEventListener('click', onDeleteHandler);
   function renderAllTasks(taskList){
     if(!taskList) {
       console.error("Передайте список задач");
@@ -56,15 +130,23 @@ const listContainer = document.querySelector('.tasks-list-section .list-group');
   renderAllTasks(objOfTasks);
   function listItemTemplate({_id, title, body} = {}){
     const li = document.createElement('li');
-    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+    li.classList.add('list-group-item', 
+                      'd-flex', 
+                      'align-items-center', 
+                      'flex-wrap', 
+                      'mt-2');
+    li.setAttribute('data-task-id', _id);
+
     const span = document.createElement('span');
     span.textContent = title;
     span.style.fontWeight = 'bold';
 
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
-    console.log(deleteBtn);
+    deleteBtn.classList.add('btn', 
+                            'btn-danger', 
+                            'ml-auto', 
+                            'delete-btn');
     const article = document.createElement('p');
     article.textContent = body;
     article.classList.add('mt-2', 'w-100');
@@ -73,5 +155,65 @@ const listContainer = document.querySelector('.tasks-list-section .list-group');
     li.appendChild(article);
 
     return li;
+    }
+    function onFormSubmitHandler(e){
+      e.preventDefault();
+      const titleValue = inputTitle.value;
+      const bodyValue = inputBody.value;
+      
+      if(!titleValue || !bodyValue){
+        alert('Пожалуйства введите title и body');
+        return;
+      }
+      const task = createNewTask(titleValue, bodyValue);
+      const listItem = listItemTemplate(task);
+      listContainer.insertAdjacentElement('afterbegin', listItem);
+      form.reset();
+    }
+    function createNewTask(title, body){
+      const newDate = new Date();
+      const newTask = {
+        title,
+        body,
+        completed: false,
+        _id: `${newDate}`
+      }
+      objOfTasks[newTask._id] = newTask;
+      return {...newTask};
+    }
+    function deleteTask(id){
+      const {title} = objOfTasks[id];
+      const isConfirm = confirm(`Точно вы хотите удалить задачу: ${title}?`);
+      if(!isConfirm) return isConfirm;
+      delete objOfTasks[id];
+      return isConfirm;
+    }
+    function deleteTaskFromHtml(confirmed, el){
+      if(!confirmed) return;
+      el.remove();
+    }
+    function onDeleteHandler({target}){
+      if(target.classList.contains('delete-btn')){
+        const parent = target.closest('[data-task-id]');
+        const id = parent.dataset.taskId;
+        const confirmed = deleteTask(id);
+        deleteTaskFromHtml(confirmed, parent);
+      }
+    }
+    function onThemeSelect(){
+      const selectedTheme = themeSelect.value;
+      const isConfirm = confirm(`Вы действительно хотите изменить тему ${selectedTheme}?`);
+      if(!isConfirm){
+        themeSelect.value = lastSelectedTheme;
+        return;
+      } 
+      setTheme(selectedTheme);
+      lastSelectedTheme = selectedTheme;
+    }
+    function setTheme(name){
+      const selectedThemeObj = themes[name];
+      Object.entries(selectedThemeObj).forEach(([key, value])=>{
+        document.documentElement.style.setProperty(key, value);
+      });
     }
 })(tasks);
